1e16c1406698f251c41ae5720f7ae53e9e1cd399
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-24 07:28:51 -0800

    feat: student scores and grade test

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -11,4 +11,12 @@ RSpec.describe Student do
     expect(@student.age).to eq 21
     expect(@student.scores).to eq []
   end
+
+  it 'logs scores and calculates grade' do
+    @student.log_score(89)
+    @student.log_score(78)
+    
+    expect(@student.scores).to eq [89, 78]
+    expect(@student.grade).to eq 83.5
+  end
 end
