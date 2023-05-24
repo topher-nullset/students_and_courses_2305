a859404d9b83d2b21a512ebf4344b20664b3ea99
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-24 07:29:19 -0800

    feat: student class methods

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
 class Student
- 
+  
+  attr_reader :name, :age, :scores
+  def initialize(stats)
+    @name = stats[:name]
+    @age = stats[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    return 0 if @scores.empty?
+    (@scores.sum.to_f / @scores.size).round(1)
+  end
 end
