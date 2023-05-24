fb7accf2ed1c259eae90b1ff68c7a98197919bf8
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-24 07:14:34 -0800

    feat: student exists test

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,5 +2,13 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
-  
+  before do
+    @student = Student.new({name: "Morgan", age:21})
+  end
+
+  it 'exists with attributes' do
+    expect(@student.name).to eq "Morgan"
+    expect(@student.age).to eq 21
+    expect(@student.scores).to eq []
+  end
 end
