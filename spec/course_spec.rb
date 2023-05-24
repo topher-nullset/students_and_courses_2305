4e913b8890c930a5bb5e5cb186e673f94c666af0
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-24 07:41:52 -0800

    feat: courses exist test

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,5 +3,14 @@ require './lib/course'
 require './lib/student'
 
 RSpec.describe Course do 
+  before do
+    @course = Course.new("Calculus", 2)
+  end
 
+  it 'exists' do
+    expect(@course.name).to eq "Calculus"
+    expect(@course.capacity).to eq 2
+    expect(@course.students).to eq []
+    expect(@course.full?).to eq false
+  end
 end
