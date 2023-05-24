989736f20c6730a0ee54352da4e2f078ef3bee23
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-24 07:49:24 -0800

    feat: course can enroll and has limit

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -13,4 +13,14 @@ RSpec.describe Course do
     expect(@course.students).to eq []
     expect(@course.full?).to eq false
   end
+
+  it 'can enroll student until full' do
+    student1 = Student.new({name: "Morgan", age: 21})
+    student2 = Student.new({name: "Jordan", age: 29})
+    @course.enroll_student student1
+    @course.enroll_student student2
+
+    expect(@course.students).to eq [student1, student2]
+    expect(@course.full?).to eq true
+  end
 end
